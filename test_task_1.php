ab114bd275bb027c8dfb45c00e22804904332bff
Muxsin <muxsin.shokirov@gmail.com>
2024-07-15 22:08:46 +0500

    Fixed test task 1

--- test_task_1.php
+++ test_task_1.php
@@ -3,26 +3,28 @@
 // Исправьте ошибки в приведенном ниже коде. Ваш исправленный код должен корректно выполнять поставленные задачи
 
 /**
-* Вычисляет факториал
-*/
-function calculateFactorial(float $number): float {
-  if ($number == 0) {
+ * Вычисляет факториал
+ */
+function calculateFactorial(int $number): int {
+  if ($number === 0) {
     return 1;
-  } else {
-    return $number * calculateFactorial($number - 1);
   }
+  
+  return $number * calculateFactorial($number - 1);
 }
 
 /**
-* Проверяет, является ли число простым
+ * Проверяет, является ли число простымS
  */
 function isPrime(int $num): bool {
   if ($num <= 1) {
     return false;
   }
 
-  for ($i = 2; $i <= sqrt($num); $i++) {
-    if ($num % $i == 0) {
+  $sqrt_num = sqrt($num);
+
+  for ($i = 2; $i <= $sqrt_num; $i++) {
+    if ($num % $i === 0) {
       return false;
     }
   }
